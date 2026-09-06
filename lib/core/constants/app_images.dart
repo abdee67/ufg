24a5124af6c842e5b9fcd8bddcccfe93c63221c0
abdee67 <alaziizz67@gmail.com>
2026-09-06 22:47:49 +0300

    fix: configure flutter_launcher_icons and update logo asset

--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -4,7 +4,7 @@ class AllImages {
   factory AllImages() => _instance;
 
   String image = 'assets/image';
-  String logo = 'assets/images/logo.jpg';
+  String logo = 'assets/images/logo.png';
   String kDefaultImage =
       'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png';
 }
